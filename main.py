d3a887e01fb410ed8b784e26c8154fd77d512351
Jean Pierre <125162733+BreadoWebTech@users.noreply.github.com>
2023-10-25 16:05:42 +0200

    Update main.py
    Added constants for better file handling
    added better file handling of the json and the proxy file
    removed the usage of globals wherever possibe and put function parameters instead
    improved the report type mapping
    improved the get_time_rn function, check_proxies_file function
    improved readabillity, maintainabillity and performance

--- main.py
+++ main.py
@@ -36,16 +36,20 @@ success = 0
 failed = 0
 generated_agents = 0
 total = 1
+config_file = "config.json"
+proxy_file = "proxies.txt"
+
+acceptable_values = ["y", "yes", "true", "1"]
 
 start = time.time()
 ctypes.windll.kernel32.SetConsoleTitleW(f'[ Tiktok MassReport ] By H4cK3dR4Du & 452b')
 
 def save_proxies(proxies):
-    with open("proxies.txt", "w") as file:
+    with open(proxy_file, "w") as file:
         file.write("\n".join(proxies))
 
 def get_proxies():
-    with open('proxies.txt', 'r', encoding='utf-8') as f:
+    with open(proxy_file, 'r', encoding='utf-8') as f:
         proxies = f.read().splitlines()
     if not proxies:
         proxy_log = {}
@@ -77,30 +81,35 @@ def get_proxies():
     except httpx.ProtocolError:
         get_proxies()
 
-def check_proxies_file():
-    file_path = "proxies.txt"
-    if os.path.exists(file_path) and os.path.getsize(file_path) == 0:
+def check_proxies_file(proxy_file):
+    if os.path.exists(proxy_file):
+        if os.path.getsize(proxy_file) == 0:
+            get_proxies()
+    else:
+        print(f"Proxy file '{proxy_file}' does not exist. Create a new proxy file.")
         get_proxies()
 
-with open(f"config.json") as f:
-    data = json.load(f)
-    if data["proxy_scraper"] == "y" or data["proxy_scraper"] == "yes":
-        check_proxies_file()
-    else:
-        pass
+try:
+     with open(config_file) as f:
+        data = json.load(f)
+        if data.get("proxy_scraper") in acceptable_values:
+            check_proxies_file()
+        else:
+            pass
+except FileNotFoundError:
+        print("Config file not found.")
+except json.JSONDecodeError:
+        print("Invalid JSON in the config file.")
 
-def update_console_title():
-    global success, failed, generated_agents, total
-    success_rate = round(success/total*100,2)
-    ctypes.windll.kernel32.SetConsoleTitleW(f'[ Tiktok MassReport ] By H4cK3dR4Du & 452b | Reports Sent : {success} ~ Failed : {failed} ~ Success Rate : {success_rate}%')
+def update_console_title(success, failed, total):
+    success_rate = round(success / total * 100, 2)
+    title = f'[ Tiktok MassReport ] By H4cK3dR4Du & 452b | Reports Sent : {success} ~ Failed : {failed} ~ Success Rate : {success_rate}%'
+    ctypes.windll.kernel32.SetConsoleTitleW(title)
+
 
 def get_time_rn():
-    date = datetime.datetime.now()
-    hour = date.hour
-    minute = date.minute
-    second = date.second
-    timee = "{:02d}:{:02d}:{:02d}".format(hour, minute, second)
-    return timee
+    timee = datetime.datetime.now().strftime("%H:%M:%S")
+    return timee 
 
 def check_ui():
     output_lock = threading.Lock()
@@ -143,44 +152,49 @@ def mass_report():
         "http": proxy_string,
         "https": proxy_string
     }
-    with open(f"config.json") as f:
-        data = json.load(f)
-        url = data['report_url']
-        report_types = data['report_types']
 
-        if report_types["Violence"] == "y" or report_types["Violence"] == "yes":
-            report_type = 90013
-        elif report_types["Sexual Abuse"] == "y" or report_types["Sexual Abuse"] == "yes":
-            report_type = 90014
-        elif report_types["Animal Abuse"] == "y" or report_types["Animal Abuse"] == "yes":
-            report_type = 90016
-        elif report_types["Criminal Activities"] == "y" or report_types["Criminal Activities"] == "yes":
-            report_type = 90017
-        elif report_types["Hate"] == "y" or report_types["Hate"] == "yes":
-            report_type = 9020
-        elif report_types["Bullying"] == "y" or report_types["Bullying"] == "yes":
-            report_type = 9007
-        elif report_types["Suicide Or Self-Harm"] == "y" or report_types["Suicide Or Self-Harm"] == "yes":
-            report_type = 90061
-        elif report_types["Dangerous Content"] == "y" or report_types["Dangerous Content"] == "yes":
-            report_type = 90064
-        elif report_types["Sexual Content"] == "y" or report_types["Sexual Content"] == "yes":
-            report_type = 90084
-        elif report_types["Porn"] == "y" or report_types["Porn"] == "yes":
-            report_type = 90085
-        elif report_types["Drugs"] == "y" or report_types["Drugs"] == "yes":
-            report_type = 90037
-        elif report_types["Firearms Or Weapons"] == "y" or report_types["Firearms Or Weapons"] == "yes":
-            report_type = 90038
-        elif report_types["Sharing Personal Info"] == "y" or report_types["Sharing Personal Info"] == "yes":
-            report_type = 9018
-        elif report_types["Human Exploitation"] == "y" or report_types["Human Exploitation"] == "yes":
-            report_type = 90015
-        elif report_types["Under Age"] == "y" or report_types["Under Age"] == "yes":
-            report_type = 91015
-    
-    output_lock = threading.Lock()
-    headers = {
+
+#    create a dictionary to map report types to report type codes so we get rid of those ugly elif statements
+    report_type_mapping = {
+    "Violence": 90013,
+    "Sexual Abuse": 90014,
+    "Animal Abuse": 90016,
+    "Criminal Activities": 90017,
+    "Hate": 9020,
+    "Bullying": 9007,
+    "Suicide Or Self-Harm": 90061,
+    "Dangerous Content": 90064,
+    "Sexual Content": 90084,
+    "Porn": 90085,
+    "Drugs": 90037,
+    "Firearms Or Weapons": 90038,
+    "Sharing Personal Info": 9018,
+    "Human Exploitation": 90015,
+    "Under Age": 91015
+    }
+
+
+    try:
+        with open(config_file) as f:
+            data = json.load(f)
+            url = data.get('report_url', '')
+            report_types = data.get('report_types', {})
+    except FileNotFoundError:
+        print("config.json not found")
+
+#   put report_type to a default value
+    report_type = None
+
+# go through all the report types and set report_type if a match is found
+    for report_type_name, report_type_code in report_type_mapping.items():
+        if report_types.get(report_type_name, "").lower() in acceptable_values:
+            report_type = report_type_code
+            break  # stop searching when report typ got matched 
+
+#   Check if report_type was set and use it
+    if report_type is not None:
+        output_lock = threading.Lock() 
+        headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36 Edg/116.0.1938.62"
     }
 
@@ -196,25 +210,16 @@ def mass_report():
             reason_number = match_reason.group(1)
             new_url = url.replace(f"reason={reason_number}", f"reason={report_type}")
             report = session.get(new_url)
-            if "Thanks for your feedback" in report.text:
+            if "Thanks for your feedback" in report.text or report.status_code == 200:
                 with output_lock:
                     time_rn = get_time_rn()
-                    print(f"[ {magenta}{time_rn}{reset} ] | ( {green}+{reset} ) {blue}Reported with successfull to ", end='')
+                    print(f"[ {magenta}{time_rn}{reset} ] | ( {green}+{reset} ) {blue}Reported with success to ", end='')
                     sys.stdout.flush()
                     Write.Print(f"{username} ~ {iduser}\n", Colors.purple_to_red, interval=0.000)
                     success += 1
                     total += 1
-                    update_console_title()
-                    mass_report()
-            elif report.status_code == 200:
-                with output_lock:
-                    time_rn = get_time_rn()
-                    print(f"[ {magenta}{time_rn}{reset} ] | ( {green}+{reset} ) {blue}Reported with successfull to ", end='')
-                    sys.stdout.flush()
-                    Write.Print(f"{username} ~ {iduser}\n", Colors.purple_to_red, interval=0.000)
-                    success += 1
-                    total += 1
-                    update_console_title()
+                    failed += 0
+                    update_console_title(success, failed, total)
                     mass_report()
             else:
                 with output_lock:
@@ -224,14 +229,16 @@ def mass_report():
                     Write.Print(f"{username} ~ {iduser}\n", Colors.purple_to_red, interval=0.000)
                     failed += 1
                     total += 1
-                    update_console_title()
+                    success += 0
+                    update_console_title(success, failed, total)
                     mass_report()
         else:
             mass_report()  
     except Exception as e:
         failed += 1
         total += 1
-        update_console_title()
+        sucess += 0
+        update_console_title(success, failed, total)
         mass_report()
 
 def mass_report_thread():
@@ -240,7 +247,15 @@ def mass_report_thread():
 def check_ui_thread():
     check_ui()
 
-num_threads = data['threads']
+try:
+    with open(config_file) as f:
+        data = json.load(f)
+        num_threads = data.get('threads', 1)  # Set a default value (1 thread) if 'threads' is not defined in the config
+except FileNotFoundError:
+    print("config.json not found")
+except json.JSONDecodeError:
+    print("Invalid JSON in the config file.")
+
 threads = []
 
 with threading.Lock():
